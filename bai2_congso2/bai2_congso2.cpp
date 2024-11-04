d54c989eef2495a1d3ccdb9e08cfbcbd5baddf9d
MAY 06 <MAY 06@LAB2-16>
2024-11-04 13:33:38 +0700

    Add project files.

---
+++ bai2_congso2/bai2_congso2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using  namespace std;
+int main() {
+	cout << "CHUONG TRINH MAY TINH TONG HAI SO " << endl;
+	int so1, so2; //khaibao bien so1 va so2 kieu so nguyen
+	int tong;
+	so1 = 3;      // gom bien so1 voi gia tri 3
+	so2 = 9;
+	tong = so1 + so2;
+	cout << "tong hai so:" << "+" << so2 << "=" << tong;
+	return 0;
+}
